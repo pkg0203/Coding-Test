95b1d928abff2571a0d9f48bc30f97f3fc95c64e
pkg0203 <pkg0203@naver.com>
2024-06-22 00:16:32 +0900

    24.06.22 ) find : argument를 찾아서 index 반환

---
+++ Baekjoon/Bronze 2/알파벳 찾기/10809.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/10809
+
+import sys
+
+ASCII_OF_a = 97
+ASCII_OF_z = 122
+
+word = sys.stdin.readline()
+answer = []
+
+# 97부터 122를 돌게 됨
+# a~z까지 word에서 찾고 answer에 append
+for walker in range(ASCII_OF_a,ASCII_OF_z+1):
+    answer.append(str(word.find(chr(walker))))
+sys.stdout.write((' ').join(answer))
