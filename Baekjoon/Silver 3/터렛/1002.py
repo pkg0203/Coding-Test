f354451a9503537861e0dad0c74824f110fc4c05
pkg0203 <pkg0203@naver.com>
2024-06-23 08:43:50 +0900

    24.06.23 ) 틀렸는데 왜 틀렸는지를 잘...

---
+++ Baekjoon/Silver 3/터렛/1002.py
@@ -0,0 +1,36 @@
+# https://www.acmicpc.net/problem/1002
+
+import sys
+
+
+def dist_square(x1, y1, x2, y2):
+    return (x1 - x2) ** 2 + (y1 - y2) ** 2
+
+
+T = int(sys.stdin.readline())
+for testcase in range(T):
+    x1, y1, r1, x2, y2, r2 = map(int, sys.stdin.readline().split())
+    d = dist_square(x1, y1, x2, y2)
+    sum_square = (r1 + r2) ** 2
+    sub_square = (r1 - r2) ** 2
+    # Same Center
+    if d == 0:
+        # Same Radius
+        if sub_square == 0:
+            result = -1
+        # Different Radius
+        else:
+            result = 0
+    # Different Center
+    else:
+        if d > sum_square:
+            result = 0
+        # Circumscription
+        elif d == sum_square or d == sub_square:
+            result = 1
+        elif d < sum_square:
+            result = 2
+        elif d < sub_square:
+            result = 0
+
+    sys.stdout.write(f"{result}\n")
