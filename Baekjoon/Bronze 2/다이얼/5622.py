9bf686acb64783193146fd4d3203b7fb63539dab
pkg0203 <pkg0203@naver.com>
2024-06-23 19:19:47 +0900

    24.06.23 ) 5622.py

---
+++ Baekjoon/Bronze 2/다이얼/5622.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/5622
+
+# https://jsikim1.tistory.com/211
+
+DIAL_CHAR = {
+    2 : "ABC",
+    3 : "DEF",
+    4 : "GHI",
+    5 : "JKL",
+    6 : "MNO",
+    7 : "PQRS",
+    8 : "TUV",
+    9 : "WXYZ",
+
+}
+
+import sys
+answer = 0
+input_char = list(sys.stdin.readline())
+for char in input_char:
+    for key in DIAL_CHAR:
+        if char in DIAL_CHAR[key]:
+            answer += (1+key)
+            break
+sys.stdout.write(f'{answer}')
