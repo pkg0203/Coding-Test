76ad22c262263dd819de87a91ea856fac61117de
pkg0203 <pkg0203@naver.com>
2024-06-23 19:39:52 +0900

    24.06.23 ) python black

--- Baekjoon/Bronze 3/그대로 출력하기/11718.py
+++ Baekjoon/Bronze 3/그대로 출력하기/11718.py
@@ -4,7 +4,7 @@ import sys
 
 while True:
     try:
-        line = sys.stdin.readline().strip()  
+        line = sys.stdin.readline().strip()
         if not line:
             break
         print(line)
