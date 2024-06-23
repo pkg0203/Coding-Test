4f69a8bb1a1a5b9c2b7b8a0e982a72a9b8816d86
pkg0203 <pkg0203@naver.com>
2024-06-24 00:51:11 +0900

    24.06.24 ) 자음 2개 이상 조건을 못 봤음

--- Baekjoon/Gold 5/암호 만들기/1759.py
+++ Baekjoon/Gold 5/암호 만들기/1759.py
@@ -2,26 +2,39 @@
 
 import sys
 
-VOWEL = ["a", "e", "i", "o", "u"]
+VOWELS = "aeiou"
+
 
 def print_answer():
     for element in answer:
-        sys.stdout.write(f'{element}\n')
+        sys.stdout.write(f"{element}\n")
 
-def exist_vowel(string_word):
-    ret = False
-    for idx in range(5):
-        if VOWEL[idx] in ''.join(string_word):
-            ret = True
-    return ret
+
+def is_valid_word(string_word):
+    # 자음 수를 세기 위함
+    consonant = 0
+    # 모음이 있는지
+    switch = False
+    for character in string_word:
+        if character in VOWELS:
+            switch = True
+        else:
+            consonant += 1
+
+    if switch:
+        if consonant >= 2:
+            return True
+    return False
+
 
 def find_answer(idx=0, partial_word=[]):
     global answer
     # Base case
     if len(partial_word) == LENGTH:
-        string_word = ''.join(partial_word)
-        if exist_vowel(string_word) and not string_word in answer:
+        string_word = "".join(partial_word)
+        if is_valid_word(string_word) and not string_word in answer:
             answer.append(string_word)
+    # Move Level
     if idx < CHAR:
         # Add Character
         partial_word.append(candidate[idx])
