43266959c3516be36d34708b1611f98940091c8f
pkg0203 <pkg0203@naver.com>
2024-06-23 19:39:19 +0900

    24.06.23 ) strip : 공백제거

---
+++ Baekjoon/Bronze 3/그대로 출력하기/11718.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/11718
+
+import sys
+
+while True:
+    try:
+        line = sys.stdin.readline().strip()  
+        if not line:
+            break
+        print(line)
+    except EOFError:
+        break
