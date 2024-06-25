a03bf74e008658a8415d6c06eb918f89ae9362b8
pkg0203 <pkg0203@naver.com>
2024-06-26 01:23:29 +0900

    24.06.26 ) 2903.py

---
+++ Baekjoon/Bronze 3/중앙 이동 알고리즘/2903.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/2903
+
+import sys
+
+A = [2,3]
+
+term = int(sys.stdin.readline())
+while True:
+    try:
+        side=A[term]
+        sys.stdout.write(f"{side**2}")
+        break
+    except :
+        A.append(A[-1]+2**(len(A)-1))
