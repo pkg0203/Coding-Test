89e68fefdbfb326028e7596529117982759f82aa
pkg0203 <pkg0203@naver.com>
2024-06-26 01:00:13 +0900

    24.06.26 ) 1929.py

---
+++ Baekjoon/Silver 3/소수 구하기/1929.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/1929
+
+import sys
+
+def is_prime(number):
+    if number != 1:
+        for walker in range(2,int(number**0.5)+1):
+            if number%walker == 0 :
+                return False
+        return True
+    # If number is 1
+    return False 
+
+M,N = map(int,sys.stdin.readline().split())
+for number in range(M,N+1):
+    if is_prime(number):
+        sys.stdout.write(f"{number}\n")
