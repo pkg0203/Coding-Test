5ea52eef5dbbe652df4a8a3eb30f13bb548f470f
pkg0203 <pkg0203@naver.com>
2024-06-26 00:52:59 +0900

    24.06.26 ) 4948.py

--- Baekjoon/Silver 2/베르트랑 공준/4948.py
+++ Baekjoon/Silver 2/베르트랑 공준/4948.py
@@ -1,60 +1,36 @@
 # https://www.acmicpc.net/problem/4948
 
-# https://nyol.tistory.com/123
+# https://velog.io/@iillyy/%EB%B0%B1%EC%A4%80-4948%EB%B2%88-%ED%8C%8C%EC%9D%B4%EC%8D%AC
 
 import sys
 
 RANGE = 123456 * 2
-PRIME_FILTER = [0] * 2 + [1] * (RANGE - 1)
+RANGE_LIST = list(range(2, RANGE + 1))
 PRIME = []
 
 
-def binary_search(number):
-    left = 0
-    right = len(PRIME) - 1
-    while left < right:
-        mid = (left + right) // 2
-        if PRIME[mid] == number:
-            return True
-        elif PRIME[mid] < number:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return False
+def prime_number(number):
+    if number == 1:
+        return False
+    # https://velog.io/@max9106/Algorithm-%EC%97%90%EB%9D%BC%ED%86%A0%EC%8A%A4%ED%85%8C%EB%84%A4%EC%8A%A4%EC%9D%98-%EC%B2%B4
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
 
 
-# walker가 PRIME일 때 PRIME의 배수를 제거하는 함수
-def update_filter(walker):
-    for i in range(2 * walker, RANGE, walker):
-        PRIME_FILTER[i] = 0
+# 입력을 받기 전에 먼저 PRIME을 구함
+for i in RANGE_LIST:
+    if prime_number(i):
+        PRIME.append(i)
 
 
-# number까지의 PRIME을 구하는 함수
-def expand_filter(number):
-    # initialize walker
-    if PRIME:
-        walker = PRIME[-1]
-    else:
-        walker = 2
-
-    while walker <= number:
-        if PRIME_FILTER[walker]:
-            PRIME.append(walker)
-            update_filter(walker)
-        walker += 1
-
-expand_filter(123456)
 while True:
-    n = int(sys.stdin.readline())
-    if n == 0:
+    input = int(sys.stdin.readline())
+    if input == 0:
         break
-    for number in range(n + 1, 2 * n + 1):
-        # 프라임에 있으면 패스
-        # if number in PRIME 을 쓰면 너무 오래 걸린다.
-        if binary_search(number):
-            pass
-        # 없으면 구한다.
-        else:
-            expand_filter(number)
-    answer = sum(PRIME_FILTER[n + 1 : 2 * n + 1])
+    answer = 0
+    for prime in PRIME:
+        if input < prime <= 2 * input:
+            answer += 1
     sys.stdout.write(f"{answer}\n")
