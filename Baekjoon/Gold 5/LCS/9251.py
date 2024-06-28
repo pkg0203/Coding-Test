47802b507d5b6982657e1758fd3272a8358ec342
pkg0203 <pkg0203@naver.com>
2024-06-28 18:28:09 +0900

    24.06.28 ) DP

--- Baekjoon/Gold 5/LCS/9251.py
+++ Baekjoon/Gold 5/LCS/9251.py
@@ -1,29 +1,8 @@
 # https://www.acmicpc.net/problem/9251
 
+# DP란?
+# https://hongjw1938.tistory.com/47
 import sys
-
-
-def find_substring(walker=0, start_idx=0):
-    global answer
-    # Base case
-    if walker == LEN_A:
-        len_str = len(string)
-        if len_str > answer:
-            answer = len_str
-        return None
-
-    matched = False
-    for idx in range(start_idx, LEN_B):
-        if A[walker] == B[idx]:
-            string.append(A[walker])
-            next_idx = idx + 1
-            matched = True
-            break
-
-    if matched:
-        find_substring(walker + 1, next_idx)
-    else:
-        find_substring(walker + 1, start_idx)
 
 
 # 개행 문자 때문에 마지막은 빼고 담아야 함
@@ -31,12 +10,15 @@ A = sys.stdin.readline()[:-1]
 B = sys.stdin.readline()[:-1]
 LEN_A, LEN_B = len(A), len(B)
 
-answer = 0
-for i in range(LEN_A):
-    string = []
-    find_substring(walker=i)
-sys.stdout.write(f"{answer}")
+# dp_table initialize
+dp_table = [[0] * (LEN_B + 1) for i in range(LEN_A + 1)]
 
-# 반례
-# XMJYAUZ
-# MZJAWXU
+for i in range(1, LEN_A + 1):
+    for j in range(1, LEN_B + 1):
+        if A[i - 1] == B[j - 1]:
+            dp_table[i][j] = dp_table[i - 1][j - 1] + 1
+        else:
+            dp_table[i][j] = max(dp_table[i - 1][j], dp_table[i][j - 1])
+
+answer = dp_table[LEN_A][LEN_B]
+sys.stdout.write(f"{answer}")
