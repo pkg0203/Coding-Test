815d999e4ed0a1764e03ac59a8dee3c5a8ea0cb0
pkg0203 <pkg0203@naver.com>
2024-07-16 23:06:00 +0900

    24.07.16 ) 부호가 음수인 경우를 고려하지 않음

--- Baekjoon/Gold 5/다항 함수의 적분/17214.py
+++ Baekjoon/Gold 5/다항 함수의 적분/17214.py
@@ -5,22 +5,35 @@ import sys
 polynomial = sys.stdin.readline()[:-1].split("+")
 antiderivative = []
 CONSTANT_OF_INTEGRATION = "W"
-for coef in polynomial:
-    if antiderivative:
-        antiderivative.append(coef + "x")
+
+for term in polynomial:
+    # Constant term
+    if term[-1] != "x":
+        antiderivative.append(term + "x")
+    # First order term
     else:
-        coef_num = coef.split("x")[0]
-        antiderivative.append(f"{int(coef_num)//2}xx")
+        coef_num = term.split("x")[0]
+        if coef_num == "":  # case when coef is "x"
+            coef_num = "1"
+        elif coef_num == "-":  # case when coef is "-x"
+            coef_num = "-1"
+        new_coef = int(coef_num) // 2
+        antiderivative.append(f"{new_coef}xx")
+
 # filtering 1
-for idx in range(2):
+for idx in range(len(antiderivative)):
     term = antiderivative[idx]
-    # eliminate x or xx
-    if idx == 0:
+    # Second order term
+    if term[-2] == "x":
         coef_num = term[:-2]
+    # First order term
     else:
         coef_num = term[:-1]
     # update antiderivative for eliminate 1
     if coef_num == "1":
         antiderivative[idx] = term[1:]
+    elif coef_num == "-1":
+        antiderivative[idx] = "-" + term[2:]
+
 antiderivative.append(CONSTANT_OF_INTEGRATION)
 sys.stdout.write(f"{'+'.join(antiderivative)}")
