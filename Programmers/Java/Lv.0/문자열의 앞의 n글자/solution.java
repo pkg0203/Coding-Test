7abf373d416d68def0a89bef431cc3fe1e464149
박강균 <141356379+pkg0203@users.noreply.github.com>
2024-10-01 06:30:13 +0900

    24.10.01 ) 문자 앞의 n글자 ( java )

---
+++ Programmers/Java/Lv.0/문자열의 앞의 n글자/solution.java
@@ -0,0 +1,10 @@
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/181907?language=java
+
+*/
+
+class Solution {
+    public String solution(String my_string, int n) {
+        return my_string.substring(0,n);
+    }
+}
