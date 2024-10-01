14fcaf605548c1e643b10d980049f685acc70a88
박강균 <141356379+pkg0203@users.noreply.github.com>
2024-10-02 01:00:52 +0900

    24.10.02 ) n의 배수(java)

---
+++ Programmers/Java/Lv.0/n의 배수/solution.java
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181937?language=java
+
+class Solution {
+    public int solution(int num, int n) {
+        return (num%n==0) ? 1 : 0;
+    }
+}
