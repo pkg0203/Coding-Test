cd8a10a2dc59353bf82888dc83f70fb3da99ae7f
박강균 <141356379+pkg0203@users.noreply.github.com>
2025-06-04 20:33:57 +0900

    25.06.04 ) 행렬 제곱

--- Baekjoon/Gold 4/행렬 제곱/10830.py
+++ Baekjoon/Gold 4/행렬 제곱/10830.py
@@ -1,36 +1,46 @@
-# https://www.acmicpc.net/problem/10830
+"""
+https://www.acmicpc.net/problem/10830
 
-import copy
-import sys
+[ Divide and Conquer ]
+이전에 풀었던 '이항 계수 3'과 크게 다르지 않다 
+다만 이 때 answer는 1이 아닌 Identity Matrix로 초기화 해야 한다 ··· (1)
 
+입력을 모두 받고서 answer로 바로 받아준다 ··· (2)
+"""
 
-def multiply(a_matrix, b_matrix):
-    new_matrix = [[0 for _ in range(N)] for _ in range(N)]
+def mat_square(A,B):
+    # (1)
+    answer = [[1 if i==j  else 0 for j in range(N)] for i in range(N)]
+    while B:
+        if B%2==1:
+            answer = multi(A,answer)
+
+        A = multi(A,A)
+        B//=2
+    return answer
+
+def multi(mat_a,mat_b):
+    answer = [[0 for _ in range(N)] for _ in range(N)]
     for i in range(N):
         for j in range(N):
             for k in range(N):
-                new_matrix[i][j] += a_matrix[i][k] * b_matrix[k][j]
-            new_matrix[i][j] %= 1000
-    return new_matrix
+                answer[i][j] += mat_a[i][k]*mat_b[k][j]
+            answer[i][j] %= R
+    return answer
 
+import sys
 
-def print_matrix(result):
-    for i in range(N):
-        sys.stdout.write(f"{' '.join(map(str,result[i]))}\n")
+# (2)
+N,B = map(int,sys.stdin.readline().split())
+A = []
+R = 1000
+for _ in range(N):
+    row = list(map(int,sys.stdin.readline().split()))
+    A.append(row)
+answer = mat_square(A,B)
+#answer = multi(A,A)
 
-
-N, power = map(int, sys.stdin.readline().split())
-matrix = []
-for idx in range(N):
-    matrix.append(list(map(int, sys.stdin.readline().split())))
-mat_expo = 1
-result = copy.deepcopy(matrix)
-
-while mat_expo < power:
-    if 2 * mat_expo <= power:
-        result = multiply(result, result)
-        mat_expo *= 2
-    else:
-        result = multiply(result, matrix)
-        mat_expo += 1
-print_matrix(result)
+for row in answer:
+    for val in row:
+        sys.stdout.write(f"{val} ")
+    sys.stdout.write("\n")
